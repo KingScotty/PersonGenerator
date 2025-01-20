5d137d33b6a16abd00a6514b894f550287331fb5
Scott Browning <scottybrowning12345@gmail.com>
2025-01-19 20:28:53 -0500

    array list and strings added

---
+++ src/PersonGenerator.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class PersonGenerator {
+    public static void main(String[] args) {
+        ArrayList<String> folks = new ArrayList<>();
+
+/*
+        a.	ID (a String)
+        b.	FirstName
+        c.	LastName
+        d.	Title (a string like Mr., Mrs., Ms., Dr., etc.)
+        e.	YearOfBirth (an int)
+*/
+
+        string ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        int YOB = 0;
+
+
+    }
+    }
